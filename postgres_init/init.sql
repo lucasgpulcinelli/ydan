52655669be5d082ac49d2b1f2b6a33e6a9fb153e
lucasgpulcinelli <lucasegp@usp.br>
2025-03-20 22:22:35 -0300

    add initial scrapelist connection and pyiceberg setup with postgres as catalog

---
+++ postgres_init/init.sql
@@ -0,0 +1,4 @@
+
+CREATE DATABASE scrapelist;
+
+CREATE DATABASE iceberg_catalog;
--- scraper/main.py
+++ scraper/main.py
@@ -1,7 +1,53 @@
 #!/usr/bin/env python3
+import os
 import time
+import psycopg2
+import pyarrow as pa
+import pyiceberg.catalog
+import pyiceberg.exceptions
 
-if __name__ == "__main__":
-    while True:
-        print("Hello, World!")
-        time.sleep(1)
+
+user = os.getenv("POSTGRES_USER", "postgres")
+password = os.getenv("POSTGRES_PASSWORD", "postgres")
+host = os.getenv("POSTGRES_HOST", "localhost")
+db = os.getenv("POSTGRES_DB", "postgres")
+port = int(os.getenv("POSTGRES_PORT", 5432))
+
+conn = psycopg2.connect(
+    user=user,
+    password=password,
+    host=host,
+    dbname=db,
+    port=port
+)
+
+c = conn.cursor()
+c.execute("SELECT table_name FROM information_schema.tables")
+print(c.fetchall())
+conn.close()
+
+catalog = pyiceberg.catalog.load_catalog("default")
+
+try:
+    catalog.create_namespace("default")
+except pyiceberg.exceptions.NamespaceAlreadyExistsError:
+    pass
+
+df = pa.Table.from_arrays(
+    [[1, 2], ["a", "b"]],
+    schema=pa.schema([
+        ("ID", pa.int64()),
+        ("NAME", pa.string())
+    ])
+)
+
+try:
+    table = catalog.create_table("default.testing", schema=df.schema)
+except pyiceberg.exceptions.TableAlreadyExistsError:
+    table = catalog.load_table("default.testing")
+
+table.append(df)
+
+print(table.scan().to_arrow())
+
+time.sleep(1000)
