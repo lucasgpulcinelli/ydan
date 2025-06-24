c3407572119af4263e297b71919839e3a5e983ba
yusukehayashibara <hayashibarayusuke0@gmail.com>
2025-06-24 16:04:30 -0300

    dimension keywords and fact subject tables

--- dbt/models/silver/keywords.sql
+++ dbt/models/silver/keywords.sql
@@ -11,7 +11,7 @@ KEYWORDS AS (
     KEYWORD_POSITION_SERIAL_NUM,
     KEYWORD_TEXT
   FROM BRONZE_LAYER.KEYWORDS
-  LIMIT 100
+  LIMIT 1000
 )
 
 SELECT
@@ -25,10 +25,4 @@ LEFT JOIN VIDEOS v
 WHERE 
     k.KEYWORD_POSITION_SERIAL_NUM > 0 AND 
     k.KEYWORD_POSITION_SERIAL_NUM < 40 AND
-    AND k.KEYWORD_TEXT RLIKE '^[\\x00-\\x7F]+$' --select only ASCII character, I made this line to avoid keywords in another languages like Japanese, Korean or Arabic
-
-
-
-
-
-
+    k.KEYWORD_TEXT RLIKE '^[\\x00-\\x7F]+$' --select only ASCII character, I made this line to avoid keywords in another languages like Japanese, Korean or Arabic
